df17e529cf630ff4a8b77716cda980ceb9ebf521
litong <ghostong@126.com>
2022-02-24 16:49:52 +0800

    暂存

--- ForwardProxy/docker-entrypoint.sh
+++ ForwardProxy/docker-entrypoint.sh
@@ -9,21 +9,21 @@ if [ "$RSA_FILEPATH" ]; then
   RSA_FILEPATH="-i "$RSA_FILEPATH
 fi
 
-#
+#代理地址
 if [ ! "$PROXY_ADDRESS" ]; then
   PROXY_ADDRESS="127.0.0.1:51234"
 fi
 
-#
+#被代理服务器地址
 if [ ! "$TARGET_ADDRESS" ]; then
   TARGET_ADDRESS="127.0.0.1:50000"
 fi
 
-#
+#SSH登录描述
 if [ ! "$LOGIN_DEPICT" ]; then
   LOGIN_DEPICT="readonly@127.0.0.1"
 fi
 
-command="${SSH} -N ${RSA_FILEPATH} -o StrictHostKeyChecking=no -L ${PROXY_ADDRESS}:${TARGET_ADDRESS} ${LOGIN_DEPICT}"
+command="${SSH} -N ${RSA_FILEPATH} -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -L ${PROXY_ADDRESS}:${TARGET_ADDRESS} ${LOGIN_DEPICT}"
 echo "${command}"
 $command
